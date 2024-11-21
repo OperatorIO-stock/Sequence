358053c2f7fe89aa1f3afad1a6b7e6984ee7136b
operatorIO-stock <vova.chigorin@yandex.com>
2024-11-21 11:40:30 +0300

    Sequence file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.IO.Pipelines;
+
+namespace Sequence;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int initialNumber = 5;
+        int finalNumber = 103;
+        
+        int stepNumber = 7;
+
+        for(int i = initialNumber; initialNumber <= finalNumber; initialNumber += stepNumber)
+        {
+            Console.WriteLine(initialNumber);
+        }
+    }
+}
